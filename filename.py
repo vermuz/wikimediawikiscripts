73b96cafc5f5e607ca6a57ff9a9eea25efd9c15e
Chaudhry Usman Ali <maniali@gmail.com>
2014-05-05 20:22:46 -0300

    Add lines to top and end of the end of file

--- filename.py
+++ filename.py
@@ -10,9 +10,15 @@ def line_prepender(filename,line):
         content = f.read()
         f.seek(0,0)
         f.write(line.rstrip('\r\n') + '\n' + content)
+def line_appender(filename,line):
+	with open(filename, "a") as f:
+		f.write(line)
+
 script, filename = argv
 filetoOpen = open(filename)
 fileName = filetoOpen.name
 file = fileName.split("_")[0]
-line = file + "\n" + "--ENDTITLE--"
-line_prepender(filename,line)
+lineattop = file + "\n" + "--ENDTITLE--"
+lineatend = "\n"+"--ENDPAGE--"+"\n"
+line_prepender(filename,lineattop)
+line_appender(filename,lineatend)
